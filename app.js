e1a8643ea31dca0a3e89950b504aac312be0b9d9
Mati_ toloo <matolox88@gmail.com>
2022-12-06 18:55:32 -0300

    Organizar codigo

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
-const {join, path} = require("path");
+const {join} = require("path");
 const session = require('express-session');
 const method = require('method-override');
 const cookie = require('cookie-parser')
 
+//server
+app.listen(3000, () => {
+    console.log('listening on port')
+})
+
+//Template Engine
+app.set("view engine", "ejs");
+app.set("views", join(__dirname, "./views"));
+
+//statics
+const {static} = require("express");
+app.use(static(join(__dirname, "./public")));
+
+//url encoded, method override 
+app.use(express.urlencoded({extended:true}));
+app.use(method('m'))
 
 app.use(session({
     secret:'ProyectoIndividual',
@@ -14,25 +30,11 @@ app.use(session({
 app.use(cookie());
 app.use(require("./middlewares/user"));
 
-app.set("views", join(__dirname, "./views"));
-
-//Template Engine
-app.set("view engine", "ejs");
-
-//statics
-const {static} = require("express");
-app.use(static(join(__dirname, "./public")));
-
 //Routers
 const peliculasRouter = require('./routes/peliculas');
 const usersRouter = require('./routes/users');
-
-app.listen(3000, () => {
-    console.log('listening on port')
-})
-
-app.use(express.urlencoded({extended:true}));
 app.use('/', peliculasRouter);
 app.use('/users', usersRouter);
+
+//json
 app.use(express.json());
-app.use(method('m'))
